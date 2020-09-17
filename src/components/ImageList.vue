acfaec11d7fa777999bfdd2b42db79c36e1387a8
veluaru <angie.castaneda@udea.edu.co>
2020-09-17 18:46:29 -0500

    Datos faltantes

---
+++ src/components/ImageList.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-card class="ma-5" max-width="600" v-bind:key="item.id" v-for="item in items.hits">
+      <v-row>
+        <v-col>
+          <img class="ma-2" :src="item.previewURL" alt />
+        </v-col>
+        <v-col>
+          <v-card-title>Propiedad de {{item.user}}</v-card-title>
+          <v-card-subtitle>Tags: {{item.tags}}</v-card-subtitle>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ImageList",
+  props: ["items"],
+};
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-app-bar color="blue lighten-34" dense dark>
+      <v-toolbar-title>Mi Galeria</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toolbar",
+};
+</script>
